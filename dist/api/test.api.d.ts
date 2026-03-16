d746956a823f925112d7bde9ebbd4e08ded4fa06
ivanjelisavcic <ivanjelisavcic6@gmail.com>
2026-03-16 10:10:49 +0100

    gitignore
    add .gitignore, remove tracked node_modules and dist

--- dist/api/test.api.d.ts
+++
@@ -1,5 +0,0 @@
-import { AxiosInstance } from 'axios';
-export declare const createTestApi: (http: AxiosInstance) => {
-    secret: () => Promise<import("axios").AxiosResponse<string, any, {}>>;
-};
-//# sourceMappingURL=test.api.d.ts.map
--- dist/api/test.api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTestApi = void 0;
-const createTestApi = (http) => ({
-    secret: () => http.get('/secret'),
-});
-exports.createTestApi = createTestApi;
-//# sourceMappingURL=test.api.js.map
